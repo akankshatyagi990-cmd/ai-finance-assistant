695dfd09c7f27bcd8dae31da6cce121c3da6ef5f
akankshatyagi990-cmd <akankshatyagi990@gmail.com>
2026-09-10 04:08:48 +0530

    Add compound interest calculation

--- app/finance.py
+++ app/finance.py
@@ -3,3 +3,12 @@ def calculate_simple_interest(principal, rate, time):
     amount = principal + interest
 
     return interest, amount
+
+def calculate_compound_interest(principal, rate, time, compounds_per_year):
+    amount = principal * (
+        1 + rate / (100 * compounds_per_year)
+    ) ** (compounds_per_year * time)
+
+    interest = amount - principal
+
+    return interest, amount
